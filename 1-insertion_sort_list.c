bdb73f48132d798330006246ac484b2c1454e133
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-22 13:49:40 +0100

    Task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp;
+
+	if (list == NULL || *list == NULL)
+		return;
+
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		temp = current;
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			if (temp->prev == *list)
+				*list = temp;
+			temp->prev->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
+			if (temp->prev != NULL)
+				temp->prev->next = temp;
+			print_list(*list);
+		}
+		current = current->next;
+	}
+}
