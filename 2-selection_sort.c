ccfe15662edb8cde6d86620a56bd64f4a0b8dfd2
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-22 14:38:20 +0100

    Task 2 (fix)

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,24 +8,26 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	int i;
-	int j;
-	int tmp;
+	int i, j, min, tmp;
 
 	if (size <= 1)
 		return;
 
 	for (i = 0; i < (int) size; i++)
 	{
+		min = i;
 		for (j = i; j < (int) size; j++)
 		{
-			if (array[j] < array[i])
-			{
-				tmp = array[i];
-				array[i] = array[j];
-				array[j] = tmp;
-				print_array(array, size);
-			}
+			if (array[j] < array[min])
+				min = j;
+		}
+
+		if (min != i)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size);
 		}
 	}
 }
